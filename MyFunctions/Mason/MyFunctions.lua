11fb06a4613696c3357c41d3cddab2bb13a0d509
Bryan Duncanson <bduncanson@madisoncollege.edu>
2021-08-13 08:11:00 -0700

    Got some time to do some work

--- MyFunctions/Mason/MyFunctions.lua
+++ MyFunctions/Mason/MyFunctions.lua
@@ -1,6 +1,5 @@
 
---Uses LIP if on CD returns false
---If buffed with LIP then will cast AOE Taunt
+--Uses LIP if on CD returns false, If buffed with LIP then will cast AOE Taunt
 --/run LipTaunt()
 function LipTaunt()
   Zorlen_useItemByName("Limited Invulnerability Potion")
@@ -8,6 +7,7 @@ function LipTaunt()
     Zorlen_castSpellByName("Challenging Shout")
   end
 end
+
 --Nice for sub 20 percent to use your rage smarter than execute.
 function PriorBT()
   local a, b, c = UnitAttackPower("player") --API function
@@ -17,5 +17,282 @@ function PriorBT()
     return true
   else
     return false
+  end
+end
+----------------------------------------------------------------------------------------
+--                                  Wrath Shout                                       --
+----------------------------------------------------------------------------------------
+
+
+--Wrath 3 piece give the player a 30 AP Bonus and can be cast out of combat then switched out giving 2 minutes of extra API
+--[[
+Attempt to clean up and explain this useful function
+
+local WrathInBags = {}
+local EquippedItems = {}
+local WrathItemEqSlot = {}
+local wrathString = "Of Wrath"
+
+--first check and create an array with current equipment
+function WrathItemsEquipped()
+  local slotId
+  local wrathCounter = 0
+  for slotId = 1,10 do --slotIds 1-10 cover all possible wrath slots
+    local itemLink = GetInventoryItemLink("player", slotId)
+    if itemLink then
+      local name, id = Zorlen_DecodeItemLink(itemLink) --all the various information about an item can be parsed out the itemLink
+      if string.find(name, WrathString) then
+        wrathCounter = wrathCounter + 1
+        if wrathCounter == 3 then
+          return true
+        end
+      end
+    end
+  end
+  return wrathCounter
+end
+
+function WrathInBags() --just looking for 3 items to get to the 30 ap bonus
+  local bag, slot, counter = WrathItemsEquipped()
+  while counter <= 3 do
+    for bag = 4, 0, -1 do --most peole sort items towards the bottom of their bags which is slot 4 on all in one bags
+      for slot 1, GetContainerNumSlots(bag) do --bag sizes vary making this api function handy
+        local itemLink = GetContainerItemLink(bag, slot) --Loop through each item slot and get the itemLink
+        if itemLink then --if the slot is not empty
+          local name, itemId = Zorlen_DecodeItemLink(itemLink) --gives infomation that can be used in other API functions
+          if name then
+            if string.find(name, WrathString) then
+              local _, _, _, _, _, _, _, eqSlot = GetItemInfo(itemId)
+              WrathInBags[eqSlot] = name
+              counter = counter + 1
+              if counter == 3 then
+                return true
+              end
+            end
+          end
+        end
+      end
+    end
+  end
+end
+
+function ItemsInvArray()
+  if not WrathInBags() and not then
+    local slotId
+    for slotId = 1, 10 do
+      local itemLink = GetInventoryItemLink("player", slotId)
+      if itemLink then
+        local name, itemId = Zorlen_DecodeItemLink(itemLink)
+        if itemId and not string.find(name, wrathString) then
+        _, _, _, _, _, _, _, eqSlot = GetItemInfo(itemId)
+          for i, v in pairs(WrathItemEqSlot) do
+            if i == eqSlot then
+              EquippedItems[eqSlot] = name
+            end
+          end
+        end
+      end
+    end
+    return true
+  end
+end
+--]]
+WratheqSlot = {}
+ItemsEquipped ={}
+wrathString = "of Wrath"
+WrathItemsEquipped = 0
+
+
+function WrathEquipped()
+  WrathItemsEquipped = 0
+  local slotId
+  for slotId = 1, 10 do
+    local itemLink = GetInventoryItemLink("player",slotId)
+    if itemLink then
+      name, id = Zorlen_DecodeItemLink(itemLink)
+      if string.find(name, wrathString) then
+        WrathItemsEquipped = WrathItemsEquipped + 1
+        if WrathItemsEquipped == 3 then
+          return WrathItemsEquipped
+        end
+      end
+    end
+  end
+  return WrathItemsEquipped
+end
+
+function isWrathEquipped()
+  if WrathItemsEquipped == 3 then
+    return true
+  else
+    return fa
+  end
+end
+
+function WrathInBags()
+  local bag, slot
+  local counter = WrathItemsEquipped
+  for bag = 4, 0, -1 do
+    for slot = 1, GetContainerNumSlots(bag) do
+      local itemLink = GetContainerItemLink(bag, slot)
+      if itemLink then
+        name, itemId = Zorlen_DecodeItemLink(itemLink)
+        if name then
+          if string.find(name, wrathString) then
+            if itemId then
+              _, _, _, _, _, _, _, eqSlot = GetItemInfo(itemId)
+              WratheqSlot[eqSlot] = name
+              counter = counter + 1
+              if counter == 3 then
+                return true
+              end
+            end
+          end
+        end
+      end
+    end
+  end
+end
+
+function ItemsInvArray()
+  local slotId
+  WrathInBags()
+  for slotId = 1, 10 do
+    local itemLink = GetInventoryItemLink("player", slotId)
+    if itemLink then
+      local name, itemId = Zorlen_DecodeItemLink(itemLink)
+      if itemId and not string.find(name, wrathString) then
+        _, _, _, _, _, _, _, eqSlot = GetItemInfo(itemId)
+        for i, v in pairs(WratheqSlot) do
+          if i == eqSlot then
+            ItemsEquipped[eqSlot] = name;
+          end
+        end
+      end
+    end
+  end
+  return true
+end
+
+function SwapInWrath()
+  WrathEquipped()
+  if not isWrathEquipped() then
+    WrathInBags()
+    if ItemsInvArray() then
+      for i, v in pairs(WratheqSlot) do
+        Zorlen_useItemByName(v)
+      end
+    end
+  end
+end
+
+function SwapBack()
+  if isWrathEquipped() then
+    for i, v in pairs(ItemsEquipped) do
+      Zorlen_useItemByName(v)
+    end
+  end
+end
+
+
+function WrathBS()
+  WrathEquipped()
+  WrathInBags()
+  ItemsInvArray()
+  local _,_,_,_,curRank = GetTalentInfo(2,15)  --returns imp zerker rage talents
+  if isWrathEquipped() then
+    if Zorlen_inCombat() then
+      if Zorlen_checkSelfBuffByName("Bloodrage") then
+        if IsShiftKeyDown() then
+          Zorlen_CancelSelfBuffByName("Bloodrage")
+        end
+      end
+    end
+  end
+
+  if IsShiftKeyDown() then
+    if Zorlen_notInCombat() and isWrathEquipped() then
+      SwapBack()
+    end
+  end
+
+  if Zorlen_notInCombat() and isBS() and (Zorlen_GetTimer("WrathBS") > 60) then
+    SwapBack()
+  end
+
+  if Zorlen_notInCombat() then
+    if isBS() and (Zorlen_GetTimer("WrathBS") < 60) or not Zorlen_IsTimer("WrathBS") then
+      Zorlen_CancelSelfBuffByName("Battle Shout")
+    end
+  end
+
+  if Zorlen_notInCombat() then
+    if not isBS() then
+      SwapInWrath()
+      if isRage(10) and isWrathEquipped() then
+        Zorlen_castSpellByName("Battle Shout");
+        Zorlen_SetTimer(120, "WrathBS")
+      elseif not isRage(10) and not curRank == 2 or not Zorlen_checkCooldownByName("Berserker Rage") then
+        Zorlen_castSpellByName("Bloodrage")
+      elseif curRank == 2 and isBerserkerStance() then
+        forceBerserkerRage()
+        Zorlen_castSpellByName("Battle Shout");
+        Zorlen_SetTimer(120, "WrathBS")
+      end
+    else
+      if isWrathEquipped() then
+        SwapBack()
+      end
+    end
+  else
+    if isRage(10) and isWrathEquipped() and (not Zorlen_IsTimer("WrathBS")) or (Zorlen_GetTimer("WrathBS") < 60) then
+      Zorlen_castSpellByName("Battle Shout");
+      Zorlen_SetTimer(120, "WrathBS")
+    end
+  end
+end
+--------------------------------------------------------------
+---             Stancing from one Key                      ---
+--------------------------------------------------------------
+StanceOption = {
+  ["tank"] = false,
+  ["dps"] = true,
+}
+--set options for
+function StanceOption:SetStance()
+  if self.tank == true then
+    self.dps = true
+    self.tank = false
+    self.string = "Defensive Stance will change to Zerker stance"
+  else
+    self.tank = true
+    self.dps = false
+    self.string = "Defensive Stance will change to Battle Stance"
+  end
+end
+
+
+function OneActionStancing()
+  if IsShiftKeyDown() then
+    if not isDefensiveStance() then
+      Zorlen_castSpellByName("Defensive Stance")
+    elseif isDefensiveStance() and UnitHealth("player") <= 60 then
+      Zorlen_useItemByName("The Face of Death")
+    end
+  elseif IsControlKeyDown() then
+    StanceOption:SetStance()
+    DEFAULT_CHAT_FRAME:AddMessage(StanceOption["string"])
+  else
+    if isDefensiveStance() then
+      if StanceOption["tank"] == true then
+        castBattleStance(test)
+      else
+        castBerserkerStance(test)
+      end
+    elseif isBattleStance() then
+      castBerserkerStance(test)
+    elseif isBerserkerStance() then
+      castBattleStance(test)
+    end
   end
 end
--- ReadMe.md
+++ ReadMe.md
@@ -1,20 +1,18 @@
 # MyFunctions
 
-Using my knowledge of the amazingly useful Zorlen Functions Library I am taking requests from 1.12.1 players on macros that
+Using the Zorlen Functions Library I am taking requests from 1.12.1 players on macros that
 they would like me to make for them.
 
-## Getting Started
+## Getting Started / Instalation
 
 **Installing Zorlen:**
 
-Outside the 1.12.1 client (before you load the game) download Zorlen from either this repo (rename if Zorlen-master to
-Zorlen) or at the website https://legacy-wow.com/vanilla-addons/zorlen/. Once downloaded extract the files contents
-(should be a folder called Zorlen) to your "*wow\Interface\AddOns*" folder.
+Outside the 1.12.1 client (before you load the game) download Zorlen from either this repo or at the website https://legacy-wow.com/vanilla-addons/zorlen/. Once downloaded extract the files contents
+ to your "*yourWoWFolder\Interface\AddOns*" folder.
 
 **Installing the MyFunctions.Lua File:**
 
-Download any of the MyFunctions.lua files located in this repo or create your own MyFunctions.lua file (copy the contents
- of various functions here to populate your file?). Once completed move/copy your MyFunctions.lua file to inside the "*./Interface/Addons/Zorlen*"
+Download any of the MyFunctions.lua files located in this repo or create your own MyFunctions.lua file. Once completed move/copy your MyFunctions.lua file to inside the "*./Interface/Addons/Zorlen*"
 
 ## Function's Format
 
@@ -29,7 +27,7 @@ Most of my Lua macros the functions will have a format similar to:
     end
 
 If this function has been properly placed inside the Zorlen/MyFunctions.lua file the foo function becomes global.
-Which means it can now be called by simply replacing the word function with /run and then ignoring everything after the ()
+Which means it can now be called from the chat frame by simply replacing the word function with /run and then ignoring everything after the ()
 in the functions name.
 
 So, the above function as text inside a macro would be as simple as:
@@ -38,8 +36,7 @@ So, the above function as text inside a macro would be as simple as:
 
 
 ##Client Instructions
-At this point you should have Zorlen installed as well as a MyFunctions.lua file (loaded with functions) inside the Zorlen
-directory. If that is true then boot up a 1.12.1 client and once you reach the character selection screen click the addon
+At this point you should have Zorlen installed as well as a MyFunctions.lua file directory. If that is true then boot up a 1.12.1 client and once you reach the character selection screen click the addon
 button in the bottom left corner and scroll down to the addon Zorlen making sure it is loaded.
 Close the addons window and then enter the world.
 
@@ -54,24 +51,18 @@ functional at this point soHotkey it or click if that's you style.
 ##About me and this project:
 I am a passionate programmer who oddly enough still gets a crazy amount of joy from writing macros for other players. Especially
 since I don’t have much time to play anymore. I decided to create this repo for the sole purpose of sharing my passion
-for writing Lua macros with the 1.12.1 community(1.12.1 > Classic - the 1.12.1 API is gold). So, to any 1.12.1 players that
-are reading feel free to send me a macro/functions you would like me to make for you and I'll get to work.
+for writing Lua macros with the 1.12.1 community. So, to any 1.12.1 players that
+are reading this feel free to send me a macro/functions you would like me to make for you and I'll get to work.
 
     Discord: BdunK#4643
 
-    or
 
-    use @Bdunk17 in an issue in this respository
-
-Will put requested functions in a file inside a folder with the name of the requestor.
+Will put requested functions in a file inside a folder with the name of the requester.
 
 I just Want to end this by say thanks to the creators of Zorlen for providing a useful library, and the players that
 continuously feed my need to write these solutions.
 
 
-
-
-*I will add pictures at some point since my dyslexic brain is way better at explanations using images
 
 
 
